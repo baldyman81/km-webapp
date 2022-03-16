f2526b808ec08532fde2c9c4c117168eae876910
baldyman81 <jamesasadler@outlook.com>
2022-03-16 20:01:34 +0800

    basic svelte version of app

--- app.js
+++
@@ -1,8 +0,0 @@
-function addBrief() {
-    let newBrief = "";
-    newBrief = document.getElementById('item');
-    console.log(newBrief.value);
-}
-
-let elAdd = document.getElementById('add');
-elAdd.addEventListener('click', addBrief, false);
---
+++ src/itemstore.js
@@ -0,0 +1,16 @@
+import { writable } from "svelte/store";
+
+export const itemsFromStore = [{
+    "name": "name value 1",
+    "id": "12",
+    "tag": "Important",
+    "publish": true
+}, {
+    "name": "A random item",
+    "id": "15",
+    "tag": "example",
+    "publish": false
+}];
+
+
+
